5624de488b38a0b0761242e0670d62667ef2c63b
wonsss <bigdatajang@gmail.com>
2022-03-22 12:17:07 +0900

    벤딩머신 설계
    Co-authored-by: 김의진 <gytks4@naver.com>

--- src/index.js
+++ src/index.js
@@ -1,51 +1,42 @@
 /**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * @class module:model/VendingMachine
+ * @classdesc
  */
-function Book(title, author) {}
+class VendingMachine {
+  /**
+   * @constructor
+   * @param {object} inventory - 상품 재고
+   * @example { iceAmericano: 7 }
+   */
+  constructor(inventory) {}
 
-/**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
- */
-function foo() {}
+  /**
+   * @type {object}
+   * @example { iceAmericano: 1000 }
+   */
+  static priceTags;
 
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
+  /** @method getItem
+   * @param {number} cash (required)
+   * @param {string} product (optional)
+   * @description 상품명과 지불금액을 입력하면 구입한 상품명과 개수가 콘솔로그에 찍힌다.
+   * @example "아메리카노 1개를 1000원에 구매했습니다.(남은재고: 9개)""
+   */
+  getItem(cash, product = "iceAmericano") {}
 
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
+  /** @method checkInventory
+   * @param {string} product (required)
+   * @param {number} quantity (required)
+   * @return {boolean}
+   * @description 소비자가 주문한 상품명과 수량을 인자로 전달하면, 해당 상품을 구입할 수 있는 재고가 있는지 boolean 반환한다.
+   */
+  #checkInventory(product, quantity) {}
 
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
-
-/**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
- */
-function Milk() {}
-
-/**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
- */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+  /** @method checkPayable
+   * @param {number} cash (required)
+   * @param {number} price (required)
+   * @return {boolean}
+   * @description 상품 가격과 지불 금액을 받아서, 구매 가능한지 boolean을 반환한다.
+   */
+  #checkPayable(cash, price) {}
+}
